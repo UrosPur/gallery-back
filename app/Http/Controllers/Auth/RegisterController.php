a66b3d24994d60893744a6d913c9f49f9155c65d
UrosPurtic <uros.purtic@gmail.com>
2018-06-11 18:57:40 +0200

    expand register controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -48,10 +48,14 @@ class RegisterController extends Controller
      */
     protected function validator(array $data)
     {
-        return Validator::make($data, [
-            'name' => 'required|string|max:255',
+        return
+
+            Validator::make($data, [
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
+            'terms' => 'required|not_in:0',
         ]);
     }
 
@@ -64,9 +68,11 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
+            'first_name' => $data['name'],
+            'last_name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'terms' => $data['terms'],
         ]);
     }
 }
