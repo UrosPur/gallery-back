8368f7e1aca8779186497a4a8c5a63e64be03eb9
uros.purtic <uros.purtic@gmail.com>
2018-06-15 08:07:45 +0200

    mora data to work with

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -6,7 +6,7 @@ $factory->define(App\Comment::class, function (Faker $faker) {
     return [
 
         'text' => $faker->text(50),
-        'gallery_id' => rand(1 , 10),
-        'user_id' => rand(1, 5)
+        'gallery_id' => rand(1 , 30),
+        'user_id' => rand(1, 10)
     ];
 });
--- database/factories/ImageFactory.php
+++ database/factories/ImageFactory.php
@@ -5,6 +5,6 @@ use Faker\Generator as Faker;
 $factory->define(App\Image::class, function (Faker $faker) {
     return [
         'url' => $faker->imageUrl($width = 220, $height = 180),
-        'gallery_id'=> rand(1,10),
+        'gallery_id'=> rand(1,30),
     ];
 });
